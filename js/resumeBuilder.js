e6703148f4b86c65ea28fec5c5affd8f3fccc81d
Dustin <dustinhorrock@gmail.com>
2015-09-11 14:55:35 -0500

    Course Work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var awesomeThoughts = "I am Dustin and I'm AWESOME";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
+
+var unformattedName = "Dustin Horrock";
+var unformattedRole = "Front End Student"
+var formattedName = HTMLheaderName.replace("%data%", unformattedName);
+var formattedRole = HTMLheaderRole.replace("%data%", unformattedRole);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var array = ["dad","mom","Jim"];
