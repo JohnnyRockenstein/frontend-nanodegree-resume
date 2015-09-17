a887716ea090813be9293332a39e353e5d5de900
Dustin <dustinhorrock@gmail.com>
2015-09-17 18:37:35 -0500

    Almost done

--- js/helper.js
+++ js/helper.js
@@ -85,7 +85,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x,y);
 });
 
 
@@ -155,7 +157,26 @@ function initializeMap() {
     var lon = placeData.geometry.location.lng();  // longitude from the place service
     var name = placeData.formatted_address;   // name of the place from the place service
     var bounds = window.mapBounds;            // current boundaries of the map window
-
+    var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">'+name+'</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>'+name+'</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
       map: map,
@@ -167,12 +188,13 @@ function initializeMap() {
     // or hover over a pin on a map. They usually contain more information
     // about a location.
     var infoWindow = new google.maps.InfoWindow({
-      content: name
+          content: contentString
     });
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+       infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,99 @@
+var bio = {
+    "name": "Dustin Horrock",
+    "role": "Content Creator",
+    "contacts": {
+        "mobile": "2048808655",
+        "email": "dustinhorrock@gmail.com",
+        "github": "JohnnyRockenstein",
+        "twitter": "@dustinhorrock",
+        "location": "Winnipeg, Manitoba"
+    },
+    "welcomeMessage": "Welcome to my page here you will find random text",
+    "skills": ["Design", "Interactive", "Retail"],
+    "biopic": "http://placehold.it/250x250",
+    "display": function() {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+        var formattedHTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+        var formattedHTMLheaderRole = HTMLheaderRole.replace("%data%", bio.role);
+        var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        var formattedHTMLbioPic = HTMLbioPic.replace("%data%", bio.biopic);
+        $("#header").prepend(formattedHTMLheaderName, formattedHTMLheaderRole);
+        $("#header").append(formattedHTMLwelcomeMsg, formattedHTMLbioPic);
+        var formattedHTMLmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        var formattedHTMLemail = HTMLemail.replace("%data%", bio.contacts.email);
+        var formattedHTMLtwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+        var formattedHTMLgithub = HTMLgithub.replace("%data%", bio.contacts.github);
+        var formattedHTMLlocation = HTMLlocation.replace("%data%", bio.contacts.location);
+        $("#topContacts").append(formattedHTMLmobile,
+            formattedHTMLemail,
+            formattedHTMLtwitter,
+            formattedHTMLgithub,
+            formattedHTMLlocation);
+        $("#header").append(HTMLskillsStart);
+        for (item in bio.skills) {
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[item]);
+            $("#skills").append(formattedSkill);
+        }
+    }
+}
+var education = {
+    "schools": [{
+        "name": "University of Manitoba",
+        "location": "Winnipeg",
+        "degree": "Bachelor Fine Art",
+        "majors": ["Design", "Photography"],
+        "dates": "September 2001 - May 2005",
+        "url": "http://www.test1.com"
+    }, {
+        "name": "University of Manitoba",
+        "location": "Winnipeg",
+        "degree": "Bachelor Fine Art Thesis",
+        "majors": ["Videography"],
+        "dates": "September 2005 - May 2006",
+        "url": "http://www.test2.com"
+    }],
+    "onlineCourses": [{
+        "title": "Javascript Basics",
+        "school": "Udacity",
+        "dates": "September 2015",
+        "url": "http://www.udacity.com/course/ud804"
+    }, {
+        "title": "Javascript Basics",
+        "school": "Udacity",
+        "dates": "September 2015",
+        "url": "http://www.udacity.com/course/ud804"
+    }],
+    "display": function() {
+        for (item in education.schools) {
+            $("#education").append(HTMLschoolStart);
+            var formattedHTMLschoolName = HTMLschoolName.replace("%data%", education.schools[item].name)
+                .replace("#", education.schools[item].url);
+            var formattedHTMLschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[item].degree);
+            var formattedNameWithDegree = formattedHTMLschoolName + formattedHTMLschoolDegree;
+            var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[item].dates);
+            var formattedHTMLschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[item].location);
+            var formattedHTMLschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[item].majors);
+            $(".education-entry:last").append(formattedNameWithDegree,
+                formattedschoolDates,
+                formattedHTMLschoolLocation,
+                formattedHTMLschoolMajor);
+        }
+        $("#education").append(HTMLonlineClasses);
+        for (item in education.onlineCourses) {
+            $("#education").append(HTMLschoolStart);
+            var formattedHTMLonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[item].title);
+            var formattedHTMLonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[item].school);
+            var formattedHTMLonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[item].dates);
+            var formattedHTMLonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[item].url);
+            var formattedTitleWithSchool = formattedHTMLonlineTitle + formattedHTMLonlineSchool;
+            $(".education-entry:last").append(formattedTitleWithSchool,
+                formattedHTMLonlineDates,
+                formattedHTMLonlineURL);
+        }
+    }
+}
+
 var work = {
     "jobs": [{
             "employer": "Canadian Tire",
@@ -30,29 +126,26 @@ var work = {
             var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[item].location)
             var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[item].description)
             $(".work-entry:last").append(formattedEmployerTitle,
-            	formattedDates,
-            	formattedLocation,
-            	formattedDescription);
+                formattedDates,
+                formattedLocation,
+                formattedDescription);
         }
     }
 }
-work.display();
-
 var projects = {
     "project": [{
-            "title": "Patio Builder",
-            "date": "November 2014 - March 2015",
-            "description": "The Patio Builder’s combination of functionality and simplicity is designed to \
+        "title": "Patio Builder",
+        "date": "November 2014 - March 2015",
+        "description": "The Patio Builder’s combination of functionality and simplicity is designed to \
             provide users with the tools to create backyard experiences in a few simple steps.",
-        }, {
-            "title": "Garage Builder",
-            "date": "June 2015 - October 2015",
-            "description": "The Garage Builder’s combination of functionality and simplicity is designed to \
-            provide users with the tools to create garage experiences in a few simple steps."
-        }
-
-    ],
-
+        "image": ["http://placehold.it/500x200", "http://placehold.it/500x200"]
+    }, {
+        "title": "Garage Builder",
+        "date": "June 2015 - October 2015",
+        "description": "The Garage Builder’s combination of functionality and simplicity is designed to \
+            provide users with the tools to create garage experiences in a few simple steps.",
+        "image": ["http://placehold.it/500x200", "http://placehold.it/500x200"]
+    }],
     "display": function() {
         for (item in projects.project) {
             $("#projects").append(HTMLprojectStart);
@@ -60,138 +153,38 @@ var projects = {
             var formattedProjectDate = HTMLprojectDates.replace("%data%", projects.project[item].date);
             var formattedProjectDesc = HTMLprojectDescription.replace("%data%", projects.project[item].description);
             $(".project-entry:last").append(formattedProjectTitle,
-            	formattedProjectDate,
-            	formattedProjectDesc);
-
+                formattedProjectDate,
+                formattedProjectDesc);
+            for (image in projects.project[item].image) {
+                var formattedHTMLprojectImage = HTMLprojectImage.replace("%data%", projects.project[item].image[image]);
+                $(".project-entry:last").append(formattedHTMLprojectImage);
+            }
         }
-
     }
 }
-projects.display();
 
-var bio = {
-    "name": "Dustin Horrock",
-    "role": "Content Creator",
-    "welcomeMessage": "Welcome to my page here you will find random text",
-    "skills": ["Design", "Interactive", "Retail"],
-    "biopic": "http://placehold.it/250x250",
-    "contacts": {
-        "mobile": "2048808655",
-        "email": "dustinhorrock@gmail.com",
-        "github": "JohnnyRockenstein",
-        "twitter": "@dustinhorrock",
-        "blog": "http://www.blogmode.com",
-        "location": "Winnipeg, Manitoba"
-    },
-    "display": function() {
-        var formattedHTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
-        var formattedHTMLheaderRole = HTMLheaderRole.replace("%data%", bio.role);
-        var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-        var formattedHTMLbioPic = HTMLbioPic.replace("%data%", bio.biopic);
-        $("#header").prepend(formattedHTMLheaderName,formattedHTMLheaderRole);
-        $("#header").append(formattedHTMLwelcomeMsg,formattedHTMLbioPic);
-        var formattedHTMLmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-        var formattedHTMLemail = HTMLemail.replace("%data%", bio.contacts.mobile);
-        var formattedHTMLtwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-        var formattedHTMLgithub = HTMLgithub.replace("%data%", bio.contacts.github);
-        var formattedHTMLblog = HTMLblog.replace("%data%", bio.contacts.blog);
-        var formattedHTMLlocation = HTMLlocation.replace("%data%", bio.contacts.location);
-        $("#topContacts").append(formattedHTMLmobile,
-        	formattedHTMLemail,
-        	formattedHTMLtwitter,
-        	formattedHTMLgithub,
-        	formattedHTMLblog,
-        	formattedHTMLlocation);
-    }
-}
 bio.display();
+work.display();
+projects.display();
+education.display();
 
+// function inName(old_name) {
+//     var newName = old_name;
+//     newName = newName.split(" ");
+//     firstName = newName[0].charAt(0).toUpperCase() + newName[0].slice(1).toLowerCase();
+//     lastName = newName[1].toUpperCase();
+//     fullName = firstName + " " + lastName;
+//     return fullName;
+// }
 
-var education = {
-    "schools": [{
-        "name": "University of Manitoba",
-        "location": "Winnipeg",
-        "degree": "Bachelor Fine Art",
-        "majors": ["Design", "Photography"],
-        "dates": "September 2001 - May 2005",
-        "url": "http://www.test1.com"
-    }, {
-        "name": "University of Manitoba",
-        "location": "Winnipeg",
-        "degree": "Bachelor Fine Art Thesis",
-        "majors": ["Videography"],
-        "dates": "September 2005 - May 2006",
-        "url": "http://www.test2.com"
-    }],
-    "onlineCourses": [{
-        "title": "Javascript Basics",
-        "location": "San Franciso",
-        "school": "Udacity",
-        "dates": "September 2015",
-        "url": "http://www.udacity.com/course/ud804"
-    }, {
-        "title": "Javascript Basics",
-        "location": "San Franciso",
-        "school": "Udacity",
-        "dates": "September 2015",
-        "url": "http://www.udacity.com/course/ud804"
-    }]
+function inName(name) {
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+	return name[0] + " " +name[1];
 }
 
-function displayEducation(){
-	for (item in education.schools) {
-		$("#education").append(HTMLschoolStart);
-		var formattedHTMLschoolName = HTMLschoolName.replace("%data%", education.schools[item].name)
-			.replace("#", education.schools[item].url);
-		var formattedHTMLschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[item].degree);
-		var formattedNameWithDegree = formattedHTMLschoolName + formattedHTMLschoolDegree;
-		var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[item].dates);
-		var formattedHTMLschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[item].location);
-		var formattedHTMLschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[item].majors);
-		$(".education-entry:last").append(formattedNameWithDegree,
-			formattedschoolDates,
-			formattedHTMLschoolLocation,
-			formattedHTMLschoolMajor);
-	}
-	$("#education").append(HTMLonlineClasses);
-	for (item in education.onlineCourses){
-		$("#education").append(HTMLschoolStart);
-		var formattedHTMLonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[item].title);
-		var formattedHTMLonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[item].school);
-		var formattedHTMLonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[item].dates);
-		var formattedHTMLonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[item].url);
-		var formattedTitleWithSchool = formattedHTMLonlineTitle + formattedHTMLonlineSchool ;
-		$(".education-entry:last").append(formattedTitleWithSchool,
-			formattedHTMLonlineDates,
-			formattedHTMLonlineURL);
-	}
-}
-displayEducation();
-
-
-
-
-if (bio.skills[0].length > 0) {
-    $("#header").append(HTMLskillsStart);
-
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-    $("#skills").append(formattedSkill);
-
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
-};
-
-function inName(old_name) {
-    var newName = old_name;
-    newName = newName.split(" ");
-    firstName = newName[0].charAt(0).toUpperCase() + newName[0].slice(1).toLowerCase();
-    lastName = newName[1].toUpperCase();
-    fullName = firstName + " " + lastName;
-    return fullName;
-}
 
 function locationizer(work) {
     var locations = [];
@@ -203,14 +196,3 @@ function locationizer(work) {
 
 $("#main").append(internationalizeButton);
 $("#mapDiv").append(googleMap);
-
-
-
-
-// for (item in work.jobs) {
-// 	var entry = $(HTMLworkStart);
-// 	$("#workExperience").append(entry);
-// 	var formattedWorkExperience = HTMLworkEmployer.replace("%data%", work.jobs[item].employer);
-// 	var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[item].title);
-// 	entry.append(formattedWorkExperience,formattedWorkTitle);
-// 	}
